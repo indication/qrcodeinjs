1cfaeb45f2df9b7a2fb2d087ce521d1359858d54
in dow <in_dow@hotmail.com>
2010-09-22 09:55:39 +0900

    refactoring loop logic.

--- bitstage.js
+++ bitstage.js
@@ -5,7 +5,6 @@ var bitstrage =  function(_packbits){
 		this.getData = function () {return this._data;};
 		this.pushData= function (_val,bitlen){
 			//エラーチェックが必要 
-			//残処理
 			if(this._restbit>0 && this._data.length > 0){
 				if(bitlen<this._restbit){
 					this._data.push(this._data.pop()|((_val > 0)?(_val<<(this._restbit-bitlen)):0));
@@ -20,18 +19,27 @@ var bitstrage =  function(_packbits){
 					this._restbit = 0;
 				}
 			}
-			//新規格納処理 
 			while(bitlen>0){
-				if(bitlen<this._packbit){
-					this._data.push(((_val > 0)?(_val<<(this._packbit-bitlen)):0));
-					this._restbit = this._packbit-bitlen;
+				var _targetBit,_lastData;
+				//残処理
+				if(this._restbit>0 && this._data.length > 0){
+					_targetBit = this._restbit;
+					_lastData = this._data.pop();
+				//新規格納処理 
+				} else {
+					_targetBit = this._packbit;
+					_lastData = 0;
+				}
+				if(bitlen<_targetBit){
+					this._data.push(_lastData|((_val > 0)?(_val<<(_targetBit-bitlen)):0));
+					this._restbit = _targetBit-bitlen;
 					bitlen = 0;
 				} else {
-					var _shift = bitlen-this._packbit;
+					var _shift = bitlen-_targetBit;
 					var _working = _val >> _shift;
 					_val = _val ^ (_working<<_shift);
-					this._data.push(_working);
-					bitlen -= this._packbit;
+					this._data.push(_lastData|_working);
+					bitlen -= _targetBit;
 					this._restbit = 0;
 				}
 			}
