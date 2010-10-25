0cf585c408f6a59226a8134574e44229750055c4
in dow <in_dow@hotmail.com>
2010-10-26 00:55:41 +0900

    インターフェイスの変更と重複コードの削除

--- bitstage.js
+++ bitstage.js
@@ -2,23 +2,8 @@ var bitstrage =  function(_packbits){
 		this._data= [];
 		this._restbit= 0;
 		this._packbit= _packbits;
-		this.getData = function () {return this._data;};
-		this.pushData= function (_val,bitlen){
-			//エラーチェックが必要 
-			if(this._restbit>0 && this._data.length > 0){
-				if(bitlen<this._restbit){
-					this._data.push(this._data.pop()|((_val > 0)?(_val<<(this._restbit-bitlen)):0));
-					this._restbit = this._restbit-bitlen;
-					bitlen = 0;
-				} else {
-					var _shift = bitlen-this._restbit;
-					var _working = _val >> _shift;
-					_val = _val ^ (_working<<_shift);
-					this._data.push(this._data.pop()|_working);
-					bitlen -= this._restbit;
-					this._restbit = 0;
-				}
-			}
+		this.get = function () {return this._data;};
+		this.push= function (_val,bitlen){
 			while(bitlen>0){
 				var _targetBit,_lastData;
 				//残処理
@@ -44,5 +29,6 @@ var bitstrage =  function(_packbits){
 				}
 			}
 			
-		};
+		}; // push
+		
 	};
